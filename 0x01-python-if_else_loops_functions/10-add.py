7144abc2504591ef8e5d8753a5a257d90d857b6a
el-armani <benjaminozor247@gmail.com>
2023-01-04 14:15:40 -0800

    added

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+# 10-add.py
+# Benjamin <benjaminozor@gmail.com>
 
 
 def add(a, b):
-
-        """Return the addition of a and b."""
-
+    """Return the addition of a and b."""
     return (a + b)
--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+# 11-pow.py
+# Benjamin <benjaminozor@gmail.com>
 
 
 def pow(a, b):
-
-        """Return a to the power of b."""
+    """Return a to the power of b."""
     return (a ** b)
