a6eab8e4d2810e8c41dd99ee46a7d9c99385184b
el-armani <benjaminozor247@gmail.com>
2023-02-17 14:36:05 -0800

    added

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# base.py
 """Defines a base model class."""
 import json
 import csv
@@ -64,7 +65,7 @@ class Base:
 
     @classmethod
     def create(cls, **dictionary):
-        """Return a class instantiated from a dictionary of attributes.
+        """Return a class instantied from a dictionary of attributes.
         Args:
             **dictionary (dict): Key/value pairs of attributes to initialize.
         """
