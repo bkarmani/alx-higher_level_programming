c540bd588fc59c903dba0973c0f2f2c6c496364b
el-armani <benjaminozor247@gmail.com>
2023-04-18 05:22:57 -0700

    made changes to script no 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,9 +2,9 @@
 import MySQLdb, sys
 def all_states (username, password, db_name):
     ports = 3306
-    db = MySQLdb.connect(host=localhost, user=username, passwd=password, db=db_name, port= ports)
+    db = MySQLdb.connect(host = 'localhost', user = username, passwd = password, db = db_name, port = ports)
     cur = db.cursor()
-    cur.execute(" SELCT * from hbtn_0e_0_usa")
+    cur.execute(" SELCT * FROM states")
     return cur.fetchone()
 if __name__ == '__main__':
     arg1 = sys.argv[1]
