461c35ac5e616e3b97ae5ba3ec8105e458ae6bf8
el-armani <benjaminozor247@gmail.com>
2023-04-18 05:13:30 -0700

    added script mo 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import MySQLdb, sys
+def all_states (username, password, db_name):
+    ports = 3306
+    db = MySQLdb.connect(host=localhost, user=username, passwd=password, db=db_name, port= ports)
+    cur = db.cursor()
+    cur.execute(" SELCT * from hbtn_0e_0_usa")
+    return cur.fetchone()
+if __name__ == '__main__':
+    arg1 = sys.argv[1]
+    arg2 = sys.argv[2]
+    arg3 = sys.argv[3]
+    print(all_states(arg1, arg2, arg3)
