d295839d076a5751f6cf224b699ee97794e8787e
el-armani <benjaminozor247@gmail.com>
2023-04-18 14:45:22 -0700

    added script no 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""  lists all states with a name starting with
+N (upper N) from the database hbtn_0e_0_usa """
+import sys
+import MySQLdb
+
+
+def ls_states(usr, ps, d):
+    """ return contents of all states in hbtn_0e_0_usa"""
+    h = 'localhost'
+    ports = 3306
+    db = MySQLdb.connect(host=h, user=usr, passwd=ps, db=d, port=ports)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    out = cur.fetchall()
+    cur.close()
+    db.close()
+    return out
+
+
+if __name__ == '__main__':
+
+    arg1 = sys.argv[1]
+    arg2 = sys.argv[2]
+    arg3 = sys.argv[3]
+    rows = ls_states(arg1, arg2, arg3)
+    for row in rows:
+        print(row)
