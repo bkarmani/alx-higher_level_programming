4097886ca314af14defa46f7d7a794c21e55b76c
el-armani <benjaminozor247@gmail.com>
2023-04-18 05:43:42 -0700

    made script 0 an executable

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+""" lists all the states contained in the database hbtn_0e_0_usa"""
 import MySQLdb, sys
 def all_states (username, password, db_name):
+    """ returns contents of the states table"""
     ports = 3306
     db = MySQLdb.connect(host = 'localhost', user = username, passwd = password, db = db_name, port = ports)
     cur = db.cursor()
-    cur.execute(" SELCT * FROM states")
+    cur.execute(" SELECT * FROM states")
     info = cur.fetchall()
     cur.close()
     db.close()
