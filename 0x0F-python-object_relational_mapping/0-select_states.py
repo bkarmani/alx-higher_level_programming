646227fa577e7d179fe400f1297bebbba50d6a42
el-armani <benjaminozor247@gmail.com>
2023-04-18 14:05:00 -0700

    made changes to script 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
 """ lists all the states contained in the database hbtn_0e_0_usa"""
-import MySQLdb, sys
-def all_states (username, password, db_name):
+import MySQLdb
+import sys
+
+
+def all_states(usr, ps, d):
     """ returns contents of the states table"""
     ports = 3306
-    db = MySQLdb.connect(host = 'localhost', user = username, passwd = password, db = db_name, port = ports)
+    h = 'localhost'
+    db = MySQLdb.connect(host=h, user=usr, passwd=ps, db=d, port=ports)
     cur = db.cursor()
     cur.execute(" SELECT * FROM states")
     info = cur.fetchall()
     cur.close()
     db.close()
     return info
+
+
 if __name__ == '__main__':
+
     arg1 = sys.argv[1]
     arg2 = sys.argv[2]
     arg3 = sys.argv[3]
